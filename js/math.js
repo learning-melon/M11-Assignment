8a183035276af4422ce58a8c8229a55249a642b6
lil-melon <133168636+lil-melon@users.noreply.github.com>
2025-01-08 23:24:44 -0800

    parts 1-2

--- js/math.js
+++ js/math.js
@@ -1,9 +1,42 @@
 //STEP 1
+let  input = prompt("Enter a number")
+while (isNaN(input)) {
+    input = prompt("Enter a number")
+}
+console.log(Math.abs(input))
 
 //STEP 2
+let input2 = prompt("Enter a decimal number")
+while (typeof input2 !== "number" && isNaN(parseFloat(input2)) && !input2.includes(".")){
+    input2 = prompt("Enter a decimal number")
+}
+console.log(Math.ceil(input2))
 
 //STEP 3
+let input3 = prompt("Enter a decimal number")
+while (typeof input3 !== "number" && isNaN(parseFloat(input2)) && !input3.includes(".")){
+    input2 = prompt("Enter a decimal number")
+}
+console.log(Math.floor(input3))
 
 //STEP 4
+let numCheck = (input) => {
+    let numArr = input.split(",")
+    for (let i = 0; i < numArr.length; i++){
+        if (isNaN(numArr[i])){
+            return false
+        }
+    }
+    return true
+}
+
+let input4 = prompt("Enter 5 numbers separated by commas")
+while(input4.split(",").length !== 5 || !numCheck(input4)){
+    input4 = prompt("Enter 5 numbers separated by commas")
+}
+console.log("Smallest: " + Math.min(...input4.split(",")))
+console.log("Largest: " + Math.max(...input4.split(",")))
 
 //STEP 5
+let input5 = prompt("Enter a number")
+console.log("Square root of " + input5 + " is " + Math.sqrt(input5))
--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,44 @@
 //STEP 1
+let name = prompt("Enter your name")
+alert(name.length)
 
 //STEP 2
+name = prompt("Enter your name")
+num = prompt("Enter a number between 0 and " + name.length)
+alert(name.charAt(num))
 
 //STEP 3
+let firstName = prompt("Enter your first name")
+let lastName  = prompt("Enter your last name")
+alert("Your name is: " + firstName.concat(" ", lastName))
 
 //STEP 4
+let storedText = "The quick brown fox jumps over the lazy dog"
+alert(storedText.indexOf("fox"))
 
 //STEP 5
+let storedText2 = "The quick brown fox jumps over the lazy fox"
+alert(storedText2.lastIndexOf("fox"))
 
 //STEP 6
+let storedText3 = "The quick brown fox jumps over the lazy dog"
+let fullName = prompt("Enter your full name")
+alert(storedText3.replace("the lazy dog", fullName))
 
 //STEP 7
+let storedText4 = "The quick brown fox jumps over the lazy dog"
+let word = prompt("Enter a word")
+alert(storedText4.search(word))
 
 //STEP 8
+let old_string = "The quick brown fox jumps over the lazy dog"
+let new_string = old_string.slice(old_string.indexOf("the lazy dog"))
+alert(new_string.toUpperCase())
 
 //STEP 9
+let storedText5 = "          THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          "
+alert(storedText5.trim().toLowerCase())
 
 //STEP 10
+let storedText6 = "the quick brown fox jumps over the lazy dog"
+alert(storedText6.charAt(0).toUpperCase() + storedText6.slice(1))
